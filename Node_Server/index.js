3a3669c3605b8ab3de39a82d0d9f3f2ff0878eed
18746 <1874689127@qq.com>
2023-07-21 22:22:21 +0800

    接口

--- Node_Server/index.js
+++ Node_Server/index.js
@@ -1,18 +1,25 @@
 const express = require('express');
 
 const localhost = require('./util/node.config');
-const url = require('./util/url');
+const apiList = require('./util/url');
 
 const app = express();
 
 app.use(express.json()); 
 
-url.list.forEach(item => {
+console.log("\n\n开始加载接口方法及路径：\n");
+
+apiList.forEach(item => {
     app[item.method](item.url, item.fn);
+    console.log('method：', item.method, '，url：', item.url);
 })
 
+console.log("\n接口方法及路径已全部加载完成\n\n");
+
+
+console.log("正在启动node服务...\n");
 app.listen(localhost.host, () => {
-  console.log('服务已启动...');
+  console.log('node服务已启动...');
   console.log(`http:localhost:${localhost.host}`);
   console.log(`http:127.0.0.1:${localhost.host}`);
 });
--- Node_Server/util/url.js
+++ Node_Server/util/url.js
@@ -1,11 +1,13 @@
-exports.list = [
+
+let apiDemoList = [
     {
         method: 'get',
         url: '/',
         fn: (req, res) => {
             console.log(req.params)
             console.log(req.query)
-            res.send({
+            res.status(200);
+            res.json({
                 a:123,b:3245
             });
         }
@@ -18,9 +20,21 @@ exports.list = [
             console.log('data', req.data)
             console.log('params', req.params)
             console.log('query', req.query)
-            res.send({
-                a:123,b:3245
+            
+            res.status(200);
+            // res.send({a:123,b:3245});
+
+            res.json({
+                data: {
+                    a:123,b:3245
+                },
+                success: true,
+                messageText: '成功'
             });
         }
     }
+]
+
+module.exports = [
+    ...apiDemoList
 ]
